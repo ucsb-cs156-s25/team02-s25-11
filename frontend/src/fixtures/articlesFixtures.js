cfe4022f0ae98f43f073ac1b42758b3ad8f76dfc
Shuang Li <shuang_li@ucsb.edu>
2025-04-30 19:31:00 -0700

    create fixtures for articles

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,40 @@
+const articlesFixtures = {
+    oneArticle: {
+        "id": 1,
+        "title": "add table",
+        "url": "https://github.com/ucsb-cs156-s25/team02-s25-11/commit/525f9901430a9c435377f833b166147fa6c04e61",
+        "explanation": "add UCSBDiningCommonsMenuItemTable, tests, and stories",
+        "email": "saul_diaz@ucsb.edu",
+        "dateAdded": "2025-05-01T02:22:14.637"
+    },
+    threeArticles: [
+        {
+          "id": 1,
+          "title": "add table",
+          "url": "https://github.com/ucsb-cs156-s25/team02-s25-11/commit/525f9901430a9c435377f833b166147fa6c04e61",
+          "explanation": "add UCSBDiningCommonsMenuItemTable, tests, and stories",
+          "email": "saul_diaz@ucsb.edu",
+          "dateAdded": "2025-05-01T02:22:14.637"
+        },
+        {
+          "id": 2,
+          "title": "error fixing",
+          "url": "https://github.com/ucsb-cs156-s25/team02-s25-11/commit/73ab79939a303a370a47fee7bf583fd104998e9a",
+          "explanation": "fix formatting in Form",
+          "email": "t_rocha@ucsb.edu",
+          "dateAdded": "2025-04-30T18:00:00"
+        },
+        {
+          "id": 3,
+          "title": "create fixtures",
+          "url": "https://github.com/ucsb-cs156-s25/team02-s25-11/commit/8926891138b12a895cde831247f0a40966604487",
+          "explanation": "create fixtures for UCSBDiningCommonsMenuItem table",
+          "email": "t_rocha@ucsb.edu",
+          "dateAdded": "2025-04-29T18:00:00"
+        }
+
+    ],
+  };
+  
+  export { articlesFixtures };
+  
