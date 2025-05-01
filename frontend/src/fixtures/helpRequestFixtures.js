b1b78b32fc1fe5310f12710e0c4e0c742b72a1cd
Luis Mendoza <luismendoza092505@gmail.com>
2025-04-30 19:47:19 -0700

    did npm run format

--- frontend/src/fixtures/helpRequestFixtures.js
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -1,5 +1,15 @@
 const helpRequestFixtures = {
-    oneHelpRequest: {
+  oneHelpRequest: {
+    id: 1,
+    requesterEmail: "cgaucho@ucsb.edu",
+    teamId: "s22-5pm-3",
+    tableOrBreakoutRoom: "7",
+    requestTime: "2022-04-20T17:35",
+    explanation: "Need help with Swagger-ui",
+    solved: "false",
+  },
+  threeHelpRequests: [
+    {
       id: 1,
       requesterEmail: "cgaucho@ucsb.edu",
       teamId: "s22-5pm-3",
@@ -8,36 +18,25 @@ const helpRequestFixtures = {
       explanation: "Need help with Swagger-ui",
       solved: "false",
     },
-    threeHelpRequests: [
-      {
-        id: 1,
-        requesterEmail: "cgaucho@ucsb.edu",
-        teamId: "s22-5pm-3",
-        tableOrBreakoutRoom: "7",
-        requestTime: "2022-04-20T17:35",
-        explanation: "Need help with Swagger-ui",
-        solved: "false",
-      },
-      {
-        id: 2,
-        requesterEmail: "ldelplaya@ucsb.edu",
-        teamId: "s22-6pm-3",
-        tableOrBreakoutRoom: "11",
-        requestTime: "2022-04-20T18:31",
-        explanation: "Dokku problems",
-        solved: "false",
-      },
-      {
-        id: 3,
-        requesterEmail: "pdg@ucsb.edu",
-        teamId: "s22-6pm-4",
-        tableOrBreakoutRoom: "13",
-        requestTime: "2022-04-21T14:15",
-        explanation: "Merge conflict",
-        solved: "false",
-      },
-    ],
-  };
-  
-  export { helpRequestFixtures };
-  
+    {
+      id: 2,
+      requesterEmail: "ldelplaya@ucsb.edu",
+      teamId: "s22-6pm-3",
+      tableOrBreakoutRoom: "11",
+      requestTime: "2022-04-20T18:31",
+      explanation: "Dokku problems",
+      solved: "false",
+    },
+    {
+      id: 3,
+      requesterEmail: "pdg@ucsb.edu",
+      teamId: "s22-6pm-4",
+      tableOrBreakoutRoom: "13",
+      requestTime: "2022-04-21T14:15",
+      explanation: "Merge conflict",
+      solved: "false",
+    },
+  ],
+};
+
+export { helpRequestFixtures };
