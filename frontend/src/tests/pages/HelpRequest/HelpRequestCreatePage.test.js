a3bf9576a6a0e640255d6107d195f0a3fced8283
Luis Mendoza <luismendoza092505@gmail.com>
2025-05-02 00:42:48 -0700

    create page for help request

--- frontend/src/tests/pages/HelpRequest/HelpRequestCreatePage.test.js
+++ frontend/src/tests/pages/HelpRequest/HelpRequestCreatePage.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, waitFor, fireEvent, screen } from "@testing-library/react";
 import HelpRequestCreatePage from "main/pages/HelpRequest/HelpRequestCreatePage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
@@ -8,10 +8,33 @@ import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 
+const mockToast = jest.fn();
+jest.mock("react-toastify", () => {
+  const originalModule = jest.requireActual("react-toastify");
+  return {
+    __esModule: true,
+    ...originalModule,
+    toast: (x) => mockToast(x),
+  };
+});
+
+const mockNavigate = jest.fn();
+jest.mock("react-router-dom", () => {
+  const originalModule = jest.requireActual("react-router-dom");
+  return {
+    __esModule: true,
+    ...originalModule,
+    Navigate: (x) => {
+      mockNavigate(x);
+      return null;
+    },
+  };
+});
+
 describe("HelpRequestCreatePage tests", () => {
   const axiosMock = new AxiosMockAdapter(axios);
 
-  const setupUserOnly = () => {
+  beforeEach(() => {
     axiosMock.reset();
     axiosMock.resetHistory();
     axiosMock
@@ -20,15 +43,10 @@ describe("HelpRequestCreatePage tests", () => {
     axiosMock
       .onGet("/api/systemInfo")
       .reply(200, systemInfoFixtures.showingNeither);
-  };
+  });
 
-  const queryClient = new QueryClient();
-  test("Renders expected content", async () => {
-    // arrange
-
-    setupUserOnly();
-
-    // act
+  test("renders without crashing", async () => {
+    const queryClient = new QueryClient();
     render(
       <QueryClientProvider client={queryClient}>
         <MemoryRouter>
@@ -37,8 +55,85 @@ describe("HelpRequestCreatePage tests", () => {
       </QueryClientProvider>,
     );
 
-    // assert
+    await waitFor(() => {
+      expect(screen.getByLabelText("Requester Email")).toBeInTheDocument();
+    });
+  });
 
-    await screen.findByText("Create page not yet implemented");
+  test("when you fill in the form and hit submit, it makes a request to the backend", async () => {
+    const queryClient = new QueryClient();
+    const helpRequest = {
+      id: 1,
+      requesterEmail: "cgaucho1@ucsb.edu",
+      teamId: "Group12-S25",
+      tableOrBreakoutRoom: "Breakout Room",
+      requestTime: "2025-05-01T00:00",
+      explanation: "Testing",
+      solved: true,
+    };
+
+    axiosMock.onPost("/api/helprequest/post").reply(202, helpRequest);
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <HelpRequestCreatePage />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
+
+    await waitFor(() => {
+      expect(screen.getByLabelText("Requester Email")).toBeInTheDocument();
+    });
+
+    const requesterEmailInput = screen.getByLabelText("Requester Email");
+    expect(requesterEmailInput).toBeInTheDocument();
+
+    const teamIdInput = screen.getByLabelText("Team ID");
+    expect(teamIdInput).toBeInTheDocument();
+
+    const tableInput = screen.getByLabelText("Table/Breakout Room");
+    expect(tableInput).toBeInTheDocument();
+
+    const requestTimeInput = screen.getByLabelText("Request Time");
+    expect(requestTimeInput).toBeInTheDocument();
+
+    const explanationInput = screen.getByLabelText("Explanation");
+    expect(explanationInput).toBeInTheDocument();
+
+    const solvedCheckbox = screen.getByLabelText("Solved");
+    expect(solvedCheckbox).toBeInTheDocument();
+
+    const createButton = screen.getByText("Create");
+    expect(createButton).toBeInTheDocument();
+
+    fireEvent.change(requesterEmailInput, {
+      target: { value: "cgaucho1@ucsb.edu" },
+    });
+    fireEvent.change(teamIdInput, { target: { value: "Group12-S25" } });
+    fireEvent.change(tableInput, { target: { value: "Breakout Room" } });
+    fireEvent.change(requestTimeInput, {
+      target: { value: "2025-05-01T00:00" },
+    });
+    fireEvent.change(explanationInput, { target: { value: "Testing" } });
+    fireEvent.click(solvedCheckbox);
+
+    fireEvent.click(createButton);
+
+    await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
+
+    expect(axiosMock.history.post[0].params).toEqual({
+      requesterEmail: "cgaucho1@ucsb.edu",
+      teamId: "Group12-S25",
+      tableOrBreakoutRoom: "Breakout Room",
+      requestTime: "2025-05-01T00:00",
+      explanation: "Testing",
+      solved: true,
+    });
+
+    expect(mockToast).toBeCalledWith(
+      "New helpRequest Created - id: 1 requesterEmail: cgaucho1@ucsb.edu",
+    );
+    expect(mockNavigate).toBeCalledWith({ to: "/helprequest" });
   });
 });
