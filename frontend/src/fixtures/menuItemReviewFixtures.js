008555d4aa9e3813547166f4c1024063128786c0
Jonah So <jonahso@ucsb.edu>
2025-05-01 17:41:33 -0700

    js - fix frontend code formatting

--- frontend/src/fixtures/menuItemReviewFixtures.js
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -1,40 +1,39 @@
 const menuItemReviewFixtures = {
-    oneMenuItemReview: [
-      {
-        id: 1,
-        itemid: 1,
-        revieweremail: "7432@ucsb.edu",
-        stars: 4,
-        comments: "good", 
-      },
-    ],
-  
-    threeMenuItemReview: [
-      {
-        id: 2,
-        itemid: 2,
-        revieweremail: "2345@ucsb.edu",
-        stars: 5,
-        comments: "delicious", 
-      },
-  
-      {
-        id: 3,
-        itemid: 3,
-        revieweremail: "7643@ucsb.edu",
-        stars: 1,
-        comments: "bad", 
-      },
-  
-      {
-        id: 4,
-        itemid: 4,
-        revieweremail: "654@ucsb.edu",
-        stars: 3,
-        comments: "okay", 
-      },
-    ],
-  };
-  
-  export { menuItemReviewFixtures };
-  
+  oneMenuItemReview: [
+    {
+      id: 1,
+      itemid: 1,
+      revieweremail: "7432@ucsb.edu",
+      stars: 4,
+      comments: "good",
+    },
+  ],
+
+  threeMenuItemReview: [
+    {
+      id: 2,
+      itemid: 2,
+      revieweremail: "2345@ucsb.edu",
+      stars: 5,
+      comments: "delicious",
+    },
+
+    {
+      id: 3,
+      itemid: 3,
+      revieweremail: "7643@ucsb.edu",
+      stars: 1,
+      comments: "bad",
+    },
+
+    {
+      id: 4,
+      itemid: 4,
+      revieweremail: "654@ucsb.edu",
+      stars: 3,
+      comments: "okay",
+    },
+  ],
+};
+
+export { menuItemReviewFixtures };
