1021bd5e8c18636a3b1f57851111dcc9f9bfeb2e
Shuang Li <shuang_li@ucsb.edu>
2025-05-02 20:27:17 -0700

    add Articles Creatr Page and tests

--- frontend/src/tests/pages/Articles/ArticlesCreatePage.test.js
+++ frontend/src/tests/pages/Articles/ArticlesCreatePage.test.js
@@ -1,17 +1,42 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import ArticlesCreatePage from "main/pages/Articles/ArticlesCreatePage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 
 import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
+
+const mockToast = jest.fn();
+jest.mock("react-toastify", () => {
+  const originalModule = jest.requireActual("react-toastify");
+  return {
+    __esModule: true,
+    ...originalModule,
+    toast: (x) => mockToast(x),
+  };
+});
+
+const mockNavigate = jest.fn();
+jest.mock("react-router-dom", () => {
+  const originalModule = jest.requireActual("react-router-dom");
+  return {
+    __esModule: true,
+    ...originalModule,
+    Navigate: (x) => {
+      mockNavigate(x);
+      return null;
+    },
+  };
+});
 
 describe("ArticlesCreatePage tests", () => {
   const axiosMock = new AxiosMockAdapter(axios);
 
-  const setupUserOnly = () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
     axiosMock.reset();
     axiosMock.resetHistory();
     axiosMock
@@ -20,15 +45,10 @@ describe("ArticlesCreatePage tests", () => {
     axiosMock
       .onGet("/api/systemInfo")
       .reply(200, systemInfoFixtures.showingNeither);
-  };
+  });
 
   const queryClient = new QueryClient();
-  test("Renders expected content", async () => {
-    // arrange
-
-    setupUserOnly();
-
-    // act
+  test("renders without crashing", async () => {
     render(
       <QueryClientProvider client={queryClient}>
         <MemoryRouter>
@@ -37,8 +57,87 @@ describe("ArticlesCreatePage tests", () => {
       </QueryClientProvider>,
     );
 
-    // assert
+    await waitFor(() => {
+      expect(screen.getByLabelText("Title")).toBeInTheDocument();
+    });
+  });
 
-    await screen.findByText("Create page not yet implemented");
+  test("on submit, makes request to backend, and redirects to /articles", async () => {
+    const queryClient = new QueryClient();
+
+    const article = {
+      id: 1,
+      title: "add table",
+      url: "https://github.com/ucsb-cs156-s25/team02-s25-11/commit/525f9901430a9c435377f833b166147fa6c04e61",
+      explanation: "add UCSBDiningCommonsMenuItemTable, tests, and stories",
+      email: "saul_diaz@ucsb.edu",
+      dateAdded: "2025-05-01T02:22:14.637",
+    };
+
+    axiosMock.onPost("/api/articles/post").reply(202, article);
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <ArticlesCreatePage />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
+
+    await waitFor(() => {
+      expect(screen.getByLabelText("Title")).toBeInTheDocument();
+    });
+
+    const titleInput = screen.getByLabelText("Title");
+    expect(titleInput).toBeInTheDocument();
+
+    const urlInput = screen.getByLabelText("Url");
+    expect(urlInput).toBeInTheDocument();
+
+    const explanationInput = screen.getByLabelText("Explanation");
+    expect(explanationInput).toBeInTheDocument();
+
+    const emailInput = screen.getByLabelText("Email");
+    expect(emailInput).toBeInTheDocument();
+
+    const dateAddedInput = screen.getByLabelText("DateAdded (iso format)");
+    expect(dateAddedInput).toBeInTheDocument();
+
+    const createButton = screen.getByText("Create");
+    expect(createButton).toBeInTheDocument();
+
+    fireEvent.change(titleInput, { target: { value: "add table" } });
+    fireEvent.change(urlInput, {
+      target: {
+        value:
+          "https://github.com/ucsb-cs156-s25/team02-s25-11/commit/525f9901430a9c435377f833b166147fa6c04e61",
+      },
+    });
+    fireEvent.change(explanationInput, {
+      target: {
+        value: "add UCSBDiningCommonsMenuItemTable, tests, and stories",
+      },
+    });
+    fireEvent.change(emailInput, { target: { value: "saul_diaz@ucsb.edu" } });
+    fireEvent.change(dateAddedInput, {
+      target: { value: "2025-05-01T02:22:14.637" },
+    });
+    fireEvent.click(createButton);
+
+    await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
+
+    expect(axiosMock.history.post[0].params).toEqual({
+      title: "add table",
+      url: "https://github.com/ucsb-cs156-s25/team02-s25-11/commit/525f9901430a9c435377f833b166147fa6c04e61",
+      explanation: "add UCSBDiningCommonsMenuItemTable, tests, and stories",
+      email: "saul_diaz@ucsb.edu",
+      dateAdded: "2025-05-01T02:22:14.637",
+    });
+
+    // assert - check that the toast was called with the expected message
+    expect(mockToast).toHaveBeenCalledWith(
+      "New articles Created - id: 1 title: add table",
+    );
+    expect(mockNavigate).toHaveBeenCalledWith({ to: "/articles" });
   });
 });
