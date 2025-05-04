b61660fb329ee078ee979708556fa187e2605e79
Jonah So <jonahso@ucsb.edu>
2025-05-04 12:00:18 -0700

    js-WIP

--- frontend/src/fixtures/menuItemReviewFixtures.js
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -1,13 +1,13 @@
 const menuItemReviewFixtures = {
-  oneMenuItemReview: [
+  oneMenuItemReview: 
     {
       id: 1,
       itemid: 1,
       revieweremail: "7432@ucsb.edu",
       stars: 4,
       comments: "good",
+      dateReviewed: "2025-01-01T00:00:00", 
     },
-  ],
 
   threeMenuItemReview: [
     {
@@ -16,6 +16,7 @@ const menuItemReviewFixtures = {
       revieweremail: "2345@ucsb.edu",
       stars: 5,
       comments: "delicious",
+      dateReviewed: "2025-01-01T00:00:00", 
     },
 
     {
@@ -24,6 +25,7 @@ const menuItemReviewFixtures = {
       revieweremail: "7643@ucsb.edu",
       stars: 1,
       comments: "bad",
+      dateReviewed: "2025-01-01T00:00:00", 
     },
 
     {
@@ -32,6 +34,7 @@ const menuItemReviewFixtures = {
       revieweremail: "654@ucsb.edu",
       stars: 3,
       comments: "okay",
+      dateReviewed: "2025-01-01T00:00:00", 
     },
   ],
 };
---
+++ frontend/src/main/components/MenuItemReview/MenuItemReviewForm.js
@@ -0,0 +1,156 @@
+import { Button, Form, Row, Col } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
+function MenuItemReviewForm({
+  initialContents,
+  submitAction,
+  buttonLabel = "Create",
+}) {
+  // Stryker disable all
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({ defaultValues: initialContents || {} });
+  // Stryker restore all
+
+  const navigate = useNavigate();
+
+  // For explanation, see: https://stackoverflow.com/questions/3143070/javascript-regex-iso-datetime
+  // Note that even this complex regex may still need some tweaks
+
+  const isodate_regex =
+    /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
+
+  const stars_regex = /^[1-5]$/;
+
+  const testIdPrefix = "MenuItemReview-";
+
+  return (
+    <Form onSubmit={handleSubmit(submitAction)}>
+      {initialContents && (
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="id">Id</Form.Label>
+          <Form.Control
+            data-testid={testIdPrefix + "id"} 
+            id="id"
+            type="text"
+            {...register("id")}
+            value={initialContents.id}
+            disabled
+          />
+        </Form.Group>
+      )}
+      <Col>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="itemid">Item ID</Form.Label>
+          <Form.Control
+            data-testid={testIdPrefix + "itemId"} 
+            id="itemid"
+            type="text"
+            {...register("itemid", {
+              required: "Item id is required.",
+            })}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.itemid?.message}
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Col>
+
+      <Col>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="revieweremail">Reviewer Email</Form.Label>
+          <Form.Control
+            data-testid={testIdPrefix + "reviewerEmail"} 
+            id="revieweremail"
+            type="text"
+            isInvalid={Boolean(errors.revieweremail)}
+            {...register("revieweremail", {
+              required: "Reviewer email is required.",
+            })}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.revieweremail?.message}
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Col>
+
+      <Col>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="stars">Stars</Form.Label>
+          <Form.Control
+            data-testid={testIdPrefix + "stars"} 
+            id="stars"
+            type="text"
+            isInvalid={Boolean(errors.stars)}
+            {...register("stars", {
+              required: true,
+              pattern: stars_regex,
+            })}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.stars && "Stars are required. "}
+            {errors.stars?.type === "pattern" &&
+              "Stars must be within the range of 1-5."}
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Col>
+
+      <Col>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="datereviewed">Date Reviewed </Form.Label>
+          <Form.Control
+            data-testid={testIdPrefix + "dateReviewed"} 
+            id="datereviewed"
+            type="datetime-local"
+            isInvalid={Boolean(errors.localDateTime)}
+            {...register("datereviewed", {
+              required: true,
+              pattern: isodate_regex,
+            })}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.datereviewed && "Date Reviewed is required. "}
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Col>
+
+      <Col>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="comments">Comments</Form.Label>
+          <Form.Control
+            data-testid={testIdPrefix + "comments"} 
+            id="comments"
+            type="text"
+            isInvalid={Boolean(errors.comments)}
+            {...register("comments", {
+              required: "Comments are required.",
+            })}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.comments?.message}
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Col>
+
+      <Row>
+        <Col>
+          <Button type="submit" data-testid="submit">
+            {buttonLabel}
+          </Button>
+          <Button
+            variant="Secondary"
+            onClick={() => navigate(-1)}
+            data-testid="cancel"
+          >
+            Cancel
+          </Button>
+        </Col>
+      </Row>
+    </Form>
+  );
+}
+
+export default MenuItemReviewForm;
