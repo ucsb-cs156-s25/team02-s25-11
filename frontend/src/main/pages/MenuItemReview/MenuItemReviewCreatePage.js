9cded439c39d0e75e1083713a85636acf4443dd6
Jonah So <jonahso@ucsb.edu>
2025-05-05 21:34:25 -0700

    js - replace MenuItemReview Create page placeholder with real create logic

--- frontend/src/main/pages/MenuItemReview/MenuItemReviewCreatePage.js
+++ frontend/src/main/pages/MenuItemReview/MenuItemReviewCreatePage.js
@@ -1,11 +1,50 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import MenuItemReviewForm from "main/components/MenuItemReview/MenuItemReviewForm";
+import { Navigate } from "react-router-dom";
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function MenuItemReviewCreatePage() {
-  // Stryker disable all : placeholder for future implementation
+export default function MenuItemReviewCreatePage({ storybook = false }) {
+  const objectToAxiosParams = (menuItemReview) => ({
+    url: "/api/menuitemreviews/post",
+    method: "POST",
+    params: {
+      itemId: menuItemReview.itemid,
+      comments: menuItemReview.comments, 
+      reviewerEmail: menuItemReview.revieweremail, 
+      stars: menuItemReview.stars, 
+      dateReviewed: menuItemReview.datereviewed
+    },
+  });
+
+  const onSuccess = (menuItemReview) => {
+    toast(
+      `New Menu Item Review Created - id: ${menuItemReview.id} itemid: ${menuItemReview.itemid}`,
+    );
+  };
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    ["/api/menuitemreviews/all"], // mutation makes this key stale so that pages relying on it reload
+  );
+
+  const { isSuccess } = mutation;
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  };
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/menuItemReview" />;
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Create page not yet implemented</h1>
+        <h1>Create New Menu Item Review</h1>
+        <MenuItemReviewForm submitAction={onSubmit} />
       </div>
     </BasicLayout>
   );
