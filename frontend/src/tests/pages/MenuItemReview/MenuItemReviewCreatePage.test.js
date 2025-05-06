c9bc8502ab9d9c2f4915697ca308616695015825
Jonah So <jonahso@ucsb.edu>
2025-05-05 23:03:10 -0700

    js - added MenuItemReview Create Page plus tests

--- frontend/src/tests/pages/MenuItemReview/MenuItemReviewCreatePage.test.js
+++ frontend/src/tests/pages/MenuItemReview/MenuItemReviewCreatePage.test.js
@@ -126,9 +126,9 @@ describe("MenuItemReviewCreatePage tests", () => {
     });
 
     // assert - check that the toast was called with the expected message
-    expect(mockToast).toHaveBeenCalledWith(
+    expect(mockToast).toBeCalledWith(
       "New Menu Item Review Created - id: 1 item id: 1 comments: good reviewer email: 7432@ucsb.edu stars: 4 date reviewed: 2025-01-01T00:00",
     );
-    expect(mockNavigate).toHaveBeenCalledWith({ to: "/menuitemreviews" });
+    expect(mockNavigate).toBeCalledWith({ to: "/menuitemreviews" });
   });
 });
